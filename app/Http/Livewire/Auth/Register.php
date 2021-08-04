068782a986c5a0b16ea5831b478f88f6fecc7d42
curder <q.curder@gmail.com>
2021-08-04 17:05:03 +0800

    Setting up the form

--- app/Http/Livewire/Auth/Register.php
+++ app/Http/Livewire/Auth/Register.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Livewire\Auth;
 
+use App\Models\User;
 use Livewire\Component;
+use Illuminate\Support\Facades\Hash;
 
 class Register extends Component
 {
+    public $email;
+    public $password;
+    public $passwordConformation;
+
+    public function register()
+    {
+        User::create([
+            'name' => $this->email,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
+    }
+
+
     public function render()
     {
         return view('livewire.auth.register');
